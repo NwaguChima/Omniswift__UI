edd162d5465ae4cec4d4745c5d64ca29efc3dfed
NwaguChima <nwaguchima48@gmail.com>
2023-03-11 11:26:04 +0100

    removed unwanted deps

---
+++ src/components/formFilter/FormFilter.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './formFilter.module.scss';
+
+interface FormFilterProps {}
+const FormFilter: React.FC<FormFilterProps> = () => {
+  return <div>Filter</div>;
+};
+
+export default FormFilter;
