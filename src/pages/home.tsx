86e443957a72e29e255bde10febbed172fefee73
NwaguChima <nwaguchima48@gmail.com>
2023-03-11 11:21:52 +0100

    setup form filter structure

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './home.module.scss';
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
-  return <div>Welcome Home</div>;
+  return (
+    <div>
+      <h1>Student Data Table</h1>
+    </div>
+  );
 };
 
 export default Home;
