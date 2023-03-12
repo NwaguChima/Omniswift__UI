2d1075ee45910120add042a53233ca9c08900af7
NwaguChima <nwaguchima48@gmail.com>
2023-03-12 01:52:56 +0100

    added table

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
+import { useTable } from 'react-table';
 import styles from './dataTable.module.scss';
 
-interface DataTableProps {}
+interface DataTableProps {
+  data: any;
+  columns: any;
+}
 
-const DataTable: React.FC<DataTableProps> = () => {
+const DataTable: React.FC<DataTableProps> = ({ data, columns }) => {
+  const tableInstance = useTable({
+    data,
+    columns,
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
   return (
     <section className={styles.dataTable}>
-      <div>Hellooooooooooo</div>
+      <table className={styles.dataTable__table} {...getTableProps()}>
+        <thead className={styles.table__head}>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody className={styles.table__body} {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+
+            return (
+              <tr className={''} {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </section>
   );
 };
